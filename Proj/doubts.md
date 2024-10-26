94b42594fa5d087ed3fca5993a567b8911030c15
AntonioAbilio <antonio.abilio2004@hotmail.com>
2024-10-26 12:16:37 +0100

    Finished LinkLayer
    Co-authored-by: vanessa-sbq <up202207919@up.pt>

--- Proj/doubts.md
+++ Proj/doubts.md
@@ -1,7 +1,10 @@
 # Doubts
 
 - If a call to write only writes partial data should we retransmit the whole frame or should we just transmit the rest that wasn't actually transmitted.
-- Should we return -1 on error when readByte() gives an error?
 - In the case where Tx has a fire and cannot send anything anymore, how will Rx know and stop ?
-- Data size in D1 and Dn (should be in application layer or link layer?)
-- Como saber quando parar de ler o D1 ... Dn?
+
+
+
+# Rx
+
+- Como saber quando parar de ler o D1 ... Dn? -> Ler tudo até à flag e verificar BCC2 no fim
--- Proj/src/link_layer.c
+++ Proj/src/link_layer.c
@@ -47,6 +47,7 @@ typedef enum {START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, STOP_STATE, CHECK_DATA} sta
 static int fd;
 static int numberOfRetransmitions = 0;
 static int timeout = 0;
+static LinkLayerRole role;
 
 // I frame number
 static int controlType = FALSE;
@@ -63,10 +64,10 @@ void alarmHandler(int signal) {
 
 
 /// Supervision frames and Unnumbered frames state machine
-int checkSUFrame(unsigned char controlField, int* ringringEnabled){
+int checkSUFrame(char controlField, int* ringringEnabled){
     state_t state = START;
     while (state != STOP_STATE && (*ringringEnabled)) {
-        unsigned char byte = 0;
+        char byte = 0;
         if (readByte(&byte) == -1) return -1;
         switch (state) {
             case START:
@@ -79,7 +80,7 @@ int checkSUFrame(unsigned char controlField, int* ringringEnabled){
                 state = byte == FLAG ? FLAG_RCV : (byte == controlField ? C_RCV : START);
                 break;
             case C_RCV:
-                unsigned char BCC1 = ADDRESS_SENT_BY_TX ^ controlField;
+                char BCC1 = ADDRESS_SENT_BY_TX ^ controlField;
                 state = byte == FLAG ? FLAG_RCV : (byte == BCC1 ? BCC_OK : START);
                 break;
             case BCC_OK:
@@ -87,8 +88,12 @@ int checkSUFrame(unsigned char controlField, int* ringringEnabled){
                 break;
             case STOP_STATE:
                 break;
+            default:
+                state = START;
+                break;
         }
     }
+    return 0;
 }
 
 
@@ -98,12 +103,13 @@ int checkSUFrame(unsigned char controlField, int* ringringEnabled){
 int llopen(LinkLayer connectionParameters) {
     numberOfRetransmitions = connectionParameters.nRetransmissions;
     timeout = connectionParameters.timeout;
+    role = connectionParameters.role;
 
     if ((fd = openSerialPort(connectionParameters.serialPort, connectionParameters.baudRate)) < 0) {
         return -1;
     }
 
-    if (connectionParameters.role == LlTx) { // Transmitter
+    if (role == LlTx) { // Transmitter
         while (alarmCount < numberOfRetransmitions) {
             int bytesWritten = 0;
 
@@ -113,7 +119,7 @@ int llopen(LinkLayer connectionParameters) {
 
                 // Assemble SET frame
                 int array_size = 5;
-                unsigned char BCC1 = ADDRESS_SENT_BY_TX ^ CONTROL_SET;
+                char BCC1 = ADDRESS_SENT_BY_TX ^ CONTROL_SET;
                 char set_array[5] = {FLAG, ADDRESS_SENT_BY_TX, CONTROL_SET, BCC1, FLAG};
 
                 // Send SET frame
@@ -132,7 +138,7 @@ int llopen(LinkLayer connectionParameters) {
                 else return 0;   
             }
         }
-    } else if (connectionParameters.role == LlRx) { // Receiver
+    } else if (role == LlRx) { // Receiver
         int enterCheckSUFrame = TRUE;
         while (enterCheckSUFrame) {
 
@@ -142,7 +148,7 @@ int llopen(LinkLayer connectionParameters) {
             }
 
             int BCC1 = ADDRESS_SENT_BY_TX ^ CONTROL_UA;
-            unsigned char ua_array[5] = {FLAG, ADDRESS_SENT_BY_TX, CONTROL_UA, BCC1, FLAG};
+            char ua_array[5] = {FLAG, ADDRESS_SENT_BY_TX, CONTROL_UA, BCC1, FLAG};
 
             int wb = writeBytes(ua_array, 5);
 
@@ -167,7 +173,7 @@ int llopen(LinkLayer connectionParameters) {
 // If it returns 0 then no errors occoured.
 int readIFrameResponse() {
     state_t state = START;
-    unsigned char BCC1 = 0x00;
+    char BCC1 = 0x00;
     char byte;
 
     while (state != STOP_STATE && alarmEnabled) {
@@ -218,6 +224,9 @@ int readIFrameResponse() {
                 break;
             case STOP_STATE:
                 break;
+            default:
+                state = START;
+                break;
         }
 
 
@@ -237,7 +246,7 @@ int llwrite(const unsigned char *buf, int bufSize) {
         if (buf[i] == FLAG || buf[i] == ESCAPE_OCTET) newFrameSize++;
     }
 
-    unsigned char* frame = (unsigned char*)malloc(sizeof(unsigned char) * (newFrameSize));
+    char* frame = (char*)malloc(sizeof(char) * (newFrameSize));
 
     frame[0] = FLAG; 
     frame[1] = ADDRESS_SENT_BY_TX;
@@ -275,7 +284,7 @@ int llwrite(const unsigned char *buf, int bufSize) {
 
     printf("\n");   // TODO: Remove
 
-    unsigned char BCC2 = buf[0];
+    char BCC2 = buf[0];
     for (int j = 1; j < bufSize; j++) {
         BCC2 ^= buf[j]; 
     }
@@ -316,6 +325,25 @@ int llwrite(const unsigned char *buf, int bufSize) {
 ////////////////////////////////////////////////
 // LLREAD
 ////////////////////////////////////////////////
+
+void sendAck(int localControlType) {
+    char RR = 0x00;
+
+    if (!localControlType) { // Inverted
+        RR = CONTROL_RR1;
+    } else {
+        RR = CONTROL_RR0;
+    }
+
+
+    char BCC1 = RR ^ ADDRESS_SENT_BY_TX;
+
+    // SEND ACK
+    char ua_array[5] = {FLAG, ADDRESS_SENT_BY_TX, RR, BCC1, FLAG};
+    int wb = writeBytes(ua_array, 5);
+}
+
+
 int llread(unsigned char *packet) {
     if (packet == NULL) {
         return -1;
@@ -326,7 +354,7 @@ int llread(unsigned char *packet) {
     int currentDataFrameIt = 0;
 
     while (state != STOP_STATE) {
-        unsigned char byte = 0;
+        char byte = 0;
         if (readByte(&byte) == -1) return -1;
 
         int controlField = controlType ? I_FRAME_1 : I_FRAME_0;
@@ -340,15 +368,24 @@ int llread(unsigned char *packet) {
                 break;
             case A_RCV:
                 state = byte == FLAG ? FLAG_RCV : (byte == controlField ? C_RCV : START);
+
+                if ((byte == I_FRAME_0 && controlType) || (byte == I_FRAME_1 && !controlType)) {
+                    sendAck(!controlType); // We want to send the old acknowledgment.
+                    state = STOP_STATE;
+                    currentDataFrameIt = 0;
+                    free(dataFrame);
+                    return 0; // Duplicate was found, we have a total of 0 bytes
+                }
+
                 break;
             case C_RCV:
-                unsigned char BCC1 = ADDRESS_SENT_BY_TX ^ controlField;
+                char BCC1 = ADDRESS_SENT_BY_TX ^ controlField;
                 state = byte == FLAG ? FLAG_RCV : (byte == BCC1 ? BCC_OK : START);
                 break;
             case BCC_OK:
                 dataFrame[currentDataFrameIt] = byte;
                 currentDataFrameIt++;
-                realloc(dataFrame, (currentDataFrameIt + 1) * sizeof(char))
+                dataFrame = (char*)realloc(dataFrame, (currentDataFrameIt + 1) * sizeof(char));
 
                 if (dataFrame == NULL || currentDataFrameIt + 1 > MAX_PAYLOAD_SIZE) return -1;
 
@@ -373,7 +410,7 @@ int llread(unsigned char *packet) {
 
                 if (expectDestuffing) {
 
-                    if ((dataFrame[i] ^ ESCAPE_XOR == ESCAPE_OCTET) || (dataFrame[i] ^ ESCAPE_XOR == FLAG)) { // Then we are really suppost to do destuffing
+                    if (((dataFrame[i] ^ ESCAPE_XOR) == ESCAPE_OCTET) || ((dataFrame[i] ^ ESCAPE_XOR) == FLAG)) { // Then we are really suppost to do destuffing
                         actualData[actualDataIt] = dataFrame[i] ^ ESCAPE_XOR;
                     } else {
                         // TODO: REMOVE
@@ -383,7 +420,7 @@ int llread(unsigned char *packet) {
                         actualData[actualDataIt] = ESCAPE_OCTET;
                         actualDataIt++;
                         sizeOfActualData++;
-                        realloc(actualData, sizeOfActualData * sizeof(char));
+                        actualData = (char*)realloc(actualData, sizeOfActualData * sizeof(char));
                         if (actualData == NULL) return -1;
 
                         // Add current byte
@@ -392,7 +429,7 @@ int llread(unsigned char *packet) {
 
                     actualDataIt++;
                     sizeOfActualData++;
-                    realloc(actualData, sizeOfActualData * sizeof(char));
+                    actualData = (char*)realloc(actualData, sizeOfActualData * sizeof(char));
                     if (actualData == NULL) return -1;
                     expectDestuffing = FALSE;
                     continue;
@@ -405,12 +442,13 @@ int llread(unsigned char *packet) {
                     actualData[actualDataIt] = dataFrame[i];
                     actualDataIt++;
                     sizeOfActualData++;
-                    realloc(actualData, sizeOfActualData * sizeof(char));
+                    actualData = (char*)realloc(actualData, sizeOfActualData * sizeof(char));
                     if (actualData == NULL) return -1;
                 }
             }
 
             // Check BCC2
+            // FIXME: What if BCC2 is equal to FLAG?
             char dataAccm = 0x00;
 
             for (int i = 0; i < sizeOfActualData; i++) {
@@ -418,22 +456,50 @@ int llread(unsigned char *packet) {
             }
 
             if (dataAccm != dataFrame[data_bcc2_flag_size - 2]) { // If dataAccm is not the same as BCC2, something went wrong
-                // Oh no :(
-                // Data is not valid :(((((((((((((((
 
+                char REJ = 0x00;
+
+                if (!controlType) {
+                    REJ = CONTROL_REJ0;
+                } else {
+                    REJ = CONTROL_REJ1;
+                }
+
+                char BCC1 = REJ ^ ADDRESS_SENT_BY_TX;
 
                 // SEND NACK
-
+                char ua_array[5] = {FLAG, ADDRESS_SENT_BY_TX, REJ, BCC1, FLAG};
+                int wb = writeBytes(ua_array, 5);
                 state = START;
 
-                // TODO: Clean allocated space ?
+                // TODO: Clean allocated space
+                free(dataFrame);
+                dataAccm = 0;
+                currentDataFrameIt = 0;
+                dataFrame = (char *)malloc(sizeof(char)); // The data from the information frame will be stored here.
+                
+                free(actualData);
+                actualData = (char*)malloc(sizeof(char));
+                actualDataIt = 0;
+                sizeOfActualData = 1;
+
+
             } else {
-                packet = 
+
+                for (int i = 0; i < actualDataIt; i++) {
+                    packet[i] = (unsigned char)actualData[i]; 
+                }
+
+                controlType = !controlType; // Frame was correctly received, tell Tx we want the next one.
+
+                sendAck(controlType); // Use normal control type. Not inverted.
+
+                state = START;
+                
                 state = STOP_STATE;
             }
         }
     }
-    // SEND ACK
 
     return 0;
 }
@@ -442,7 +508,72 @@ int llread(unsigned char *packet) {
 // LLCLOSE
 ////////////////////////////////////////////////
 int llclose(int showStatistics) {
-    // TODO
+    if (role == LlTx) { // Transmitter
+        while (alarmCount < numberOfRetransmitions) {
+            int bytesWritten = 0;
+
+            if (alarmEnabled == FALSE){
+                alarm(timeout); // Set alarm to be triggered after timeout
+                alarmEnabled = TRUE;
+
+                // Assemble DISC frame
+                int array_size = 5;
+                char BCC1 = ADDRESS_SENT_BY_TX ^ CONTROL_DISC;
+                char set_array[5] = {FLAG, ADDRESS_SENT_BY_TX, CONTROL_DISC, BCC1, FLAG};
+
+                // Send DISC frame
+                while (bytesWritten != 5) { // FIXME: Remove hard-coded value?
+                    bytesWritten = writeBytes((set_array + sizeof(char) * bytesWritten), array_size - bytesWritten);
+                    if (bytesWritten == -1) {
+                        return -1;
+                    }
+                }
+                sleep(1); // Wait until all bytes have been written to the serial port
+            }
+
+            if (bytesWritten == 5) {
+                int csu = checkSUFrame(CONTROL_DISC, &alarmEnabled);
+                if (csu == -1) return -1;
+                else return 1;   
+            }
+        }
+
+
+        // Is this correct ??????????
+        while (1) {
+            int csu = checkSUFrame(CONTROL_UA, &alarmEnabled);
+            if (csu == -1) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+
+    } else if (role == LlRx) { // Receiver
+        int enterCheckSUFrame = TRUE;
+        while (enterCheckSUFrame) {
+
+            int csu = checkSUFrame(CONTROL_DISC, &alarmEnabled);
+            if (csu == -1) {
+                return -1;
+            }
+
+            int BCC1 = ADDRESS_SENT_BY_TX ^ CONTROL_DISC;
+            char ua_array[5] = {FLAG, ADDRESS_SENT_BY_TX, CONTROL_DISC, BCC1, FLAG};
+
+            int wb = writeBytes(ua_array, 5);
+
+            if (wb == -1) {
+                return -1;
+            } else if (wb == 5) {
+                return 1;
+            } else {
+                continue;
+            }
+        }
+    }
+
+    return 1;
 
     int clstat = closeSerialPort();
     return clstat;
